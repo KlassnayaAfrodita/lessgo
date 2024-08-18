0c9d9aa500d71652108a7a1d8bed7d442a7736a1
hokamsingh <hokamsingh.mongoosetech@gmail.com>
2024-08-18 23:22:38 +0530

    feat: using dig for di

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,11 +3,6 @@ package utils
 import (
 	"os"
 	"path/filepath"
-
-	"github.com/hokamsingh/lessgo/internal/core/controller"
-	"github.com/hokamsingh/lessgo/internal/core/di"
-	"github.com/hokamsingh/lessgo/internal/core/module"
-	"github.com/hokamsingh/lessgo/internal/core/router"
 )
 
 func GetFolderPath(folderName string) (string, error) {
@@ -28,14 +23,14 @@ func GetFolderPath(folderName string) (string, error) {
 	return folderPath, nil
 }
 
-func RegisterModuleControllers(r *router.Router, container *di.Container, module module.Module) error {
-	for _, ctrl := range module.Controllers {
-		if c, ok := ctrl.(controller.Controller); ok {
-			// Inject dependencies
-			container.Inject(c)
-			// Register routes
-			c.RegisterRoutes(r.Mux)
-		}
-	}
-	return nil
-}
+// func RegisterModuleControllers(r *router.Router, container *di.Container, module module.Module) error {
+// 	for _, ctrl := range module.Controllers {
+// 		if c, ok := ctrl.(controller.Controller); ok {
+// 			// Inject dependencies
+// 			container.Inject(c)
+// 			// Register routes
+// 			c.RegisterRoutes(r.Mux)
+// 		}
+// 	}
+// 	return nil
+// }
